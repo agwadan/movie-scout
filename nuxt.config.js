063f7b99a0c5d02f3597e4b0148cb3fca95c8cef
agwadan <agwa.dan@gmail.com>
2025-04-07 22:56:17 +0300

    Update rating to appear to one decimal place

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'movie-app',
+    title: 'Movie Scout',
     htmlAttrs: {
       lang: 'en'
     },
